389ab761d7361785764c3ef49c895b06395f937a
Saqel <clohik94@mail.ru>
2014-08-17 14:05:04 +0400

    Functions
    Added functions to check filled fields.
    Testing only.

--- cgi-bin/check.py
+++ cgi-bin/check.py
@@ -1,18 +1,44 @@
 # -*-coding: utf-8 -*-
 
-import os,sys,cgi
+import os
+import sys
+import cgi
+import re
 location = os.getcwd()
 
 file = open('%s/check-data.html' % location, 'r')
 HTML = file.read()
 file.close()
 
+def checPhone(phone):
+	return re.match('[+][0-9]{1,3}[-][0-9]{3}[-][0-9]{3}[-][0-9]{2}[-][0-9]{2}', phone) is not None 
+	
+def checkEmail(email):
+	return re.match('', email) is not None
 
+def checkFirstName(firstName):
+	return re.match('', firsName) is not None
+
+def checkSecondName(secondName):
+	return re.match('', seconName) is not None
+	
 index = 0
 form = cgi.FieldStorage()
 names = ('firstName', 'secondName', 'phoneNumber', 'email', 'birthday', 'comment')
 forReplace = ('$FIRST$', '$SECOND$', '$PHONE$', '$EMAIL$', '$BIRTHDAY$', '$TEXTAREA$')
 for name in names:
+	'''
+	if name[0]:
+	        if checkFirstName(firstName) == True:
+	                continue
+	        else:
+	                break
+	if name[2]:
+		if checkPhone(phone) == True:
+			continue
+		else:
+			break
+	'''
 	Field = name[0].upper() + name[1:]
 	Field = cgi.escape(form['%s' % name].value)
 	HTML = HTML.replace('%s' % forReplace[index], Field)
